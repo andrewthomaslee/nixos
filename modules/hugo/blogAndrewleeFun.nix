3d72bfe0a56e9843b044119e54c0f61670b561c7
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-01-14 20:35:30 -0600

    nfc

--- modules/hugo/blogAndrewleeFun.nix
+++ modules/hugo/blogAndrewleeFun.nix
@@ -1,12 +1,12 @@
 {
   config,
   lib,
-  inputs,
   pkgs,
+  blogAndrewleeFun,
   ...
 }: let
   cfg = config.clan-net.services.hugo.blogAndrewleeFun;
-  static-files = inputs."blogAndrewleeFun".packages.${pkgs.stdenv.hostPlatform.system}.default;
+  static-files = blogAndrewleeFun.packages.${pkgs.stdenv.hostPlatform.system}.default;
 in {
   options.clan-net.services.hugo.blogAndrewleeFun.enable = lib.mkEnableOption "blog.andrewlee.fun";
 
