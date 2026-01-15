69cc957b30fcd54f58b48c091f39987b2947d4a9
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-01-15 10:47:57 -0600

    testing clan's postgres intergration

--- machines/helsinki-vps/configuration.nix
+++ machines/helsinki-vps/configuration.nix
@@ -11,6 +11,27 @@
     };
   };
 
+  # Enable the PostgreSQL backup module
+  clan.core.postgresql.enable = true;
+  clan.core.postgresql.users.test = {
+    name = "test";
+  };
+  # Configure each database
+  clan.core.postgresql.databases.test = {
+    # Database creation options (runs on first setup)
+    name = "test";
+    create = {
+      enable = true;
+      options = {
+        ENCODING = "UTF8";
+        LC_COLLATE = "C";
+        LC_CTYPE = "C";
+        OWNER = "test";
+        TEMPLATE = "template0";
+      };
+    };
+  };
+
   # User Profiles
   home-manager.users.netsa = flake-self.homeConfigurations.server;
 }
