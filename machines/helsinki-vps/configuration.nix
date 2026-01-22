50f36fa408e39eb6af1bfbe37b5b1463ea2eeb45
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-01-22 07:57:00 -0600

    trying headscale

--- machines/helsinki-vps/configuration.nix
+++ machines/helsinki-vps/configuration.nix
@@ -8,6 +8,14 @@
         enable = true;
         blogAndrewleeFun.enable = true;
       };
+      headscale = {
+        enable = true;
+        base_domain = "netsam.dev";
+        server_url = "vpn.netsam.dev";
+        adminUser = "andrewthomaslee@netsam.com";
+        client_id = "768131783288-cdrtl1ahhltlgcpl42pqdohabf0q623v.apps.googleusercontent.com";
+        allowed_domains = ["netsam.com"];
+      };
     };
   };
 
