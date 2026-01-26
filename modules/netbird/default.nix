6d1ef3be7c599dfdc6ab95fa85be9fa11ec4d362
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-01-26 15:19:52 -0600

    import sub modules

--- modules/netbird/default.nix
+++ modules/netbird/default.nix
@@ -5,6 +5,11 @@
 }: let
   cfg = config.clan-net.networking.netbird;
 in {
+  imports = [
+    ./clan-net.nix
+    ./industrial-host.nix
+  ];
+
   options.clan-net.networking.netbird = {
     enable = lib.mkEnableOption "netbird";
     role = lib.mkOption {
