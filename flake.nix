2415f774ac98d0fe93d60b0b5998234dfbe08c9d
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-01-28 09:31:57 -0600

    x86_64-linux only

--- flake.nix
+++ flake.nix
@@ -47,9 +47,6 @@
       # System types to support.
       supportedSystems = [
         "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
       ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
