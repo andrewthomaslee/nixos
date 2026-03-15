eb66d0d36fb06ced167b13bcd5d32021cc779715
andrewthomaslee <andrewthomaslee.business@gmail.com>
2026-03-15 18:30:24 -0500

    allow ping

--- modules/clan-common/default.nix
+++ modules/clan-common/default.nix
@@ -17,4 +17,9 @@
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28="
   ];
+
+  networking.firewall = {
+    enable = lib.mkDefault true;
+    allowPing = lib.mkDefault true;
+  };
 }
